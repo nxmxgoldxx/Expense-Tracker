cb93dd0fec79f77f5161ba4f2dcf979230d7740d
Calenté Cardwell <nxmxgoldxx@live.com>
2021-07-22 19:38:39 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const button = document.querySelector(".show-results");
+const results = document.querySelector(".results");
+const expensesList = document.querySelector(".expenses");
+
+const expenses = [
+  9.99,
+  25.8,
+  144.39,
+  12.98,
+  4.99,
+  38.75,
+  14.5,
+  99.99,
+  75.4,
+  4.75,
+  62.88
+];
+
+// Display your expenses in a list
+for (let exp of expenses) {
+  let li = document.createElement("li");
+  li.innerText = `$ ${exp}`;
+  expensesList.append(li);
+}
+
+// Write showMinMax function here
+
+const showMinMax = function (expenses) {
+  const min = Math.min(...expenses);
+  const max = Math.max(...expenses);
+
+  const minListItem = document.createElement("li");
+  minListItem.innerText = `Min: ${min}`;
+
+  const maxListItem = document.createAttribute.createElement("li");
+
+  results.append(minListItem);
+  results.append(maxListItem);
+
+  results.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+  showMinMax(expenses);
+  button.disabled = true;
+});
+
+// Write click event listener for 'button' and call the showMinMax function
